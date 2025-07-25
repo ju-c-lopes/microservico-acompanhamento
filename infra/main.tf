3de05708593c4a8b23c591bc7b776f1aa31e922b
Juliano <juliano.co.lopes@gmail.com>
2025-07-25 17:54:48 -0300

    fix: update db_subnet_group_name for RDS instance

--- infra/main.tf
+++ infra/main.tf
@@ -94,6 +94,6 @@ resource "aws_db_instance" "mysql" {
     parameter_group_name   = "default.mysql8.0"
     skip_final_snapshot    = true
     publicly_accessible    = true
-    db_subnet_group_name   = aws_db_subnet_group.default.name
+    db_subnet_group_name   = aws_db_subnet_group.rds.name
     vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
