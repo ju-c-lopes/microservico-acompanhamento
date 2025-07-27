0e52dc1f5987840f86859099d8a6238d4912c1b0
Juliano <juliano.co.lopes@gmail.com>
2025-07-27 09:13:39 -0300

    fix: enhance deployment workflow

--- alembic/env.py
+++ alembic/env.py
@@ -17,8 +17,9 @@ target_metadata = Base.metadata
 def run_migrations_online():
     """Run migrations in 'online' mode."""
     db_url = os.environ.get("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
-    if db_url is not None and db_url.startswith("mysql+aiomysql"):
+    if db_url.startswith("mysql+aiomysql"):
         db_url = db_url.replace("mysql+aiomysql", "mysql+pymysql")
+    print(f"\nUsing database URL: {db_url}\n")
     connectable = create_engine(db_url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
