0bef50c9283fda39cfd8ecf400ad48512f06682d
Juliano <juliano.co.lopes@gmail.com>
2025-07-26 21:13:44 -0300

    fix: update database URL handling for MySQL in migration script

--- alembic/env.py
+++ alembic/env.py
@@ -17,6 +17,8 @@ target_metadata = Base.metadata
 def run_migrations_online():
     """Run migrations in 'online' mode."""
     db_url = os.environ.get("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
+    if db_url is not None and db_url.startswith("mysql+aiomysql"):
+        db_url = db_url.replace("mysql+aiomysql", "mysql+pymysql")
     connectable = create_engine(db_url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
